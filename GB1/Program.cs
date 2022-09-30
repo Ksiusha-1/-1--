b5c3e6e2d31f4cfdbe3ca80757950f99f6965d7b
Ksiuni Lime <possible.live@bk.ru>
2022-09-30 22:41:13 +0300

    пример задачи 5

---
+++ GB1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите  число");
+int X = int.Parse("Console.Readline()!");
+int N2 = -X;
+while (N2 <= X )
+{
+ Console.Write($" {N2} ");
+ N2 = N2 + 1;
+
+}
+
+
