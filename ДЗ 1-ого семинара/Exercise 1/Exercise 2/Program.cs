d76f508ba6570387f9f8de543feaae7767afc5ad
Ksiuni Lime <possible.live@bk.ru>
2022-10-03 23:02:57 +0300

    Задача 4 решение

---
+++ ДЗ 1-ого семинара/Exercise 1/Exercise 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 2;
+int b = 5;
+int c = 7;
+
+int max = a;
+
+if(a > max ) max = a ;
+if(b > max ) max = b ;
+if(c > max ) max = c ;
+
+Console.Write("max= ") ;
+Console.WriteLine(max) ;
--- ДЗ 1-ого семинара/Exercise 1/Program.cs
+++ ДЗ 1-ого семинара/Exercise 1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Введите два числа ");
-Console.Write("Введите первое число : 5 ");
+Console.Write("Введите первое число : 5 !");
 int a = int.Parse(Console.ReadLine ()!);
-Console.Write("Введите второе число : 7 ");
+Console.Write("Введите второе число : 7 ! ");
 int b = int.Parse(Console.ReadLine ()!);
 Console.WriteLine($"Вычислить какое из двух чисел (max), а какое (min)");
  if (a<b)
